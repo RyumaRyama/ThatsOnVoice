e303f3c8a7eb029b6ee5fa68ef8206db9ffb1ee0
RyumaRyama <ryumaryama@gmail.com>
2024-04-21 20:52:00 +0900

    マテリアルデザイン設定と必要ボタンなどの追加

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,14 +12,47 @@ using System.Windows.Shapes;
 
 namespace ThatsOnVoice
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private MicVolume mv;
         public MainWindow()
         {
             InitializeComponent();
+
+            mv = new MicVolume();
+            DataContext = mv;
+            Volume_Mock();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // ダイアログのインスタンスを生成
+            var dialog = new OpenFileDialog();
+
+            // ファイルの種類を設定
+            dialog.Filter = "雑音|*.mp3";
+
+            // ダイアログを表示する
+            if (dialog.ShowDialog() == true)
+            {
+                // 選択されたファイル名 (ファイルパス) をメッセージボックスに表示
+                MessageBox.Show(dialog.FileName);
+            }
+        }
+
+        private void Volume_Mock()
+        {
+            Random rand = new Random();
+            //while (true)
+            //{
+            mv.volume_value = rand.Next(1, 100);
+            //await Task.Delay(10);
+            //}
+        }
+
+        private void ToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ MicVolume.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThatsOnVoice
+{
+    class MicVolume
+    {
+        public int volume_value { get; set; }
+    }
+}
+
